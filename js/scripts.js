8b479c8affa7ce23f95875e3397cc80426af6bc5
Jenny Landrum <jennylandrum@gmail.com>
2019-07-24 23:19:35 -0500

    added repository objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 var repository = ['jigglypuff', 'litwick', 'mrMime']
+  var jigglypuff ={
+    name: 'Jigglypuff',
+    height: .5,
+    types: ['fairy', 'normal']
+  };
+  var litwick ={
+    name: 'Litwick',
+    height: .3,
+    types: ['fire', 'ghost']
+  };
+  var mrMime ={
+    name: 'Mr. Mime',
+    height: 1.3,
+    types: ['psychic', 'fairy']
+  };
