5accefa263632fbd3d7ba1031f8deb619007e135
Jenny Landrum <jennylandrum@gmail.com>
2019-07-24 23:19:59 -0500

    looping and writing to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,7 @@ var repository = ['jigglypuff', 'litwick', 'mrMime']
     height: 1.3,
     types: ['psychic', 'fairy']
   };
+
+for (var i = 0; i < repository.length; i++) {
+  document.write('<p>' + name + ' height:' + height + '</p>')
+}
