2ea3889fd2c95f935aa1ede34dfcbc2649ddf5ae
Jenny Landrum <jennylandrum@gmail.com>
2019-07-25 19:16:21 -0500

    corrected the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
-var repository = ['jigglypuff', 'litwick', 'mrMime'];
-  var jigglypuff ={
+var repository = [
+  {
     name: 'Jigglypuff',
     height: .5,
     types: ['fairy', 'normal']
-  };
+  },
   var litwick ={
     name: 'Litwick',
     height: .3,
     types: ['fire', 'ghost']
-  };
+  },
   var mrMime ={
     name: 'Mr. Mime',
     height: 1.3,
     types: ['psychic', 'fairy']
-  };
-
+  }
+]
 for (var i = 0; i < repository.length; i++) {
-  document.write('<p>' + name + ' height: ' + height + '</p>')
+  var pokemon = i
+  document.write('<p>' + repository.name + ' (height: ' + repository.height + ')</p>')
 };
